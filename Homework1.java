27fbaefb12282db3e55c48e4fd7b35db6164ea2d
kangho <kil0653@naver.com>
2025-09-11 15:10:36 +0900

    열심히했습니다.

---
+++ Homework1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Homework1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+        for (int i = 0; i < 5; i++) {
+            int num = sc.nextInt();
+            sum += num;
+        }
+        System.out.println(sum);
+    }
+}
